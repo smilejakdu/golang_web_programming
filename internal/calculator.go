14b8cc77fa273591f390705212d9257799e5ea3d
moonyoung <moonyoung.dev@kakao.com>
2022-07-09 01:25:45 +0900

    feat: 뺄셈 구현

--- internal/calculator.go
+++ internal/calculator.go
@@ -10,3 +10,7 @@ func NewCalculator() *Calculator {
 func (c Calculator) Add(num1, num2 int) int {
 	panic("implement me")
 }
+
+func (c Calculator) Minus(num1, num2 int) int {
+	return num1 - num2
+}
